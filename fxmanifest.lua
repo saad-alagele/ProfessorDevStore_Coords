72d34bc9f1e6ce3c0437e85aefc5c09c53f8163f
Saad Alagele <127995823+saad-alagele@users.noreply.github.com>
2026-02-03 13:13:52 +0300

    تنسيق بسيط

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Saad / ProfessorDev'
+description 'Professor Coords Panel'
+version '1.0.0'
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/app.js',
+    'ui/style.css'
+}
+
+client_script 'client.lua'
+
+/*
+ProfessorDiv Store — by Saad alagele - سعد العجيلي | Discord: 1pd
+
+تحذير:
+يُمنع منعًا باتًا فك الحماية أو تعديل التشفير أو إعادة نشر/بيع السكربت دون إذن خطي.
+أي انتهاك يُعد خيانة للأمانة وأكلًا للمال بالباطل.
+
+{ وَلَا تَأْكُلُوا أَمْوَالَكُم بَيْنَكُم بِالْبَاطِلِ } (البقرة: 188)
+*/
